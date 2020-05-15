bb722d3bb2cc669dfa245b3e83c2990d7d3c4171
jaeju.jang <jjj0611@gmail.com>
2020-05-15 09:41:55 +0900

    [init] initialize spring project to practice

---
+++ src/test/java/org/backend/master/springpractice/SpringPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.backend.master.springpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
